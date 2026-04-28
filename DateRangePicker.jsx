db541b7220120681b0409e06bcca77c54423d07c
Muhammad Azhar <wattooazhar758@gmail.com>
2026-04-28 18:22:28 +0500

    Implement Custom date range picker component for Reports page

---
+++ DateRangePicker.jsx
@@ -0,0 +1,13 @@
+// Custom date range picker component
+import React from 'react';
+
+const DateRangePicker = () => {
+    return (
+        <div>
+            <h2>Select Date Range</h2>
+            {/* Date range picker implementation */}
+        </div>
+    );
+};
+
+export default DateRangePicker;
